5a29c05bd4d181cd20b1c116d8c28499ef3486fd
Hubert Wang <hubert.tatra@gmail.com>
2018-12-20 11:57:29 +0700

    Add assets and layout adjustment

---
+++ NotificationService-HandsOn/Controller/ShadowView.swift
@@ -0,0 +1,23 @@
+//
+//  ShadowView.swift
+//  NotificationService-HandsOn
+//
+//  Created by Hubert Wang on 20/12/2018.
+//  Copyright © 2018 Hubert Wang. All rights reserved.
+//
+
+import UIKit
+
+class ShadowView: UIView {
+    
+    override func awakeFromNib() {
+        layer.shadowPath = CGPath(rect: layer.bounds, transform: nil)
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 1, height: 1)
+        layer.shadowOpacity = 0.2
+        layer.shadowRadius = 5
+        
+        layer.cornerRadius = 5
+    }
+
+}
