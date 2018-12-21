bc634f5dd27202912ec1528a9e98e88b3b35c236
Hubert Wang <hubert.tatra@gmail.com>
2018-12-21 21:55:24 +0700

    Add NotificationAttachmentId enum

---
+++ NotificationService-HandsOn/Model/NotificationAttachmentId.swift
@@ -0,0 +1,15 @@
+//
+//  NotificationAttachmentId.swift
+//  NotificationService-HandsOn
+//
+//  Created by Hubert Wang on 21/12/18.
+//  Copyright © 2018 Hubert Wang. All rights reserved.
+//
+
+import Foundation
+
+enum NotificationAttachmentId: String {
+    case timer = "userNotification.attachment.timer"
+    case date = "userNotification.attachment.date"
+    case location = "userNotification.attachment.location"
+}
