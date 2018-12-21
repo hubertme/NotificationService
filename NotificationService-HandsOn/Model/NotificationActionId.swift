ddc0c4112d6f5fa4b58d59ff5ca420ea83e6ade0
Hubert Wang <hubert.tatra@gmail.com>
2018-12-21 22:11:49 +0700

    Add NotificationActionId and NotificationCategoryId

---
+++ NotificationService-HandsOn/Model/NotificationActionId.swift
@@ -0,0 +1,15 @@
+//
+//  NotificationActionId.swift
+//  NotificationService-HandsOn
+//
+//  Created by Hubert Wang on 21/12/18.
+//  Copyright © 2018 Hubert Wang. All rights reserved.
+//
+
+import Foundation
+
+enum NotificationActionId: String {
+    case timer = "userNotification.action.timer"
+    case date = "userNotification.action.date"
+    case location = "userNotification.action.location"
+}
---
+++ NotificationService-HandsOn/Model/NotificationCategoryId.swift
@@ -0,0 +1,15 @@
+//
+//  NotificationCategory.swift
+//  NotificationService-HandsOn
+//
+//  Created by Hubert Wang on 21/12/18.
+//  Copyright © 2018 Hubert Wang. All rights reserved.
+//
+
+import Foundation
+
+enum NotificationCategoryId: String {
+    case timer = "userNotification.category.timer"
+    case date = "userNotification.category.date"
+    case location = "userNotification.category.location"
+}
